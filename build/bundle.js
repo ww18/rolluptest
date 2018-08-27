6e2adb8921b6c0d8981a7c77a6142e83f57a17e5
liwenwen <liwenwen@oak-inc.com>
2018-08-27 15:28:35 +0800

    Initial commit

---
+++ build/bundle.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// maths.js
+
+// This function gets included
+function cube ( x ) {
+	// rewrite this as `square( x ) * x`
+	// and see what happens!
+	return x * x * x;
+}
+
+/* TREE-SHAKING */
+
+console.log( cube( 5 ) ); // 125
+//# sourceMappingURL=bundle.js.map
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default{
+	input: "src/main.js",
+	output:{
+		"sourcemap": true,
+		"file": "build/bundle.js",
+		"format": 'cjs'
+	}
+}
